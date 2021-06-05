e221beaf26473035e2e4e3657e60270ba5bcfb04
kanoonsantikul <nitisantikul@gmail.com>
2021-06-05 14:32:50 +0700

    add file to migrate database

---
+++ migration/initialize.js
@@ -0,0 +1,36 @@
+const sqlite3 = require("sqlite3").verbose();
+
+let db = new sqlite3.Database(".db/calvary19.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("Connected to the database.");
+  }
+});
+
+db.run(
+  `CREATE TABLE IF NOT EXISTS vehicle (
+      id TEXT PRIMARY KEY,
+      type TEXT NOT NULL,
+      status TEXT NOT NULL,
+      regimental TEXT,
+      serial_no TEXT,
+      created_date TEXT NOT NULL,
+      created_by TEXT NOT NULL,
+      updated_date TEXT,
+      updated_by TEXT
+  )`,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+  }
+);
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("Close the database connection.");
+  }
+});
---
+++ start_dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir .db
+touch .db/calvary19.db
+node ./migration/initialize.js
