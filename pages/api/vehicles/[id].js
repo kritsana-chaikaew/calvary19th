1edceb62b98a19e3a62f72ba42d4bd90a4f2dca3
Kritsana <kritsana.chai@ku.th>
2021-06-05 22:02:44 +0700

    delete vehicle

--- pages/api/vehicles/[id].js
+++ pages/api/vehicles/[id].js
@@ -13,26 +13,35 @@ export async function getVehicle(id) {
   return vehicle;
 }
 
+export async function deleteVehicle(id) {
+  const db = new sqlite3.Database(".db/calvary19.db");
+  const asyncRun = promisify(db.run).bind(db);
+  const sql = "DELETE FROM vehicle WHERE id=?";
+  await asyncRun(sql, [id]);
+  db.close();
+}
+
 export default async function handler(req, res) {
   const {
     query: { id },
     method,
   } = req;
 
-  switch (method) {
-    case "GET":
-      const vehicle = await getVehicle(id);
-      if (vehicle) {
+  const vehicle = await getVehicle(id);
+  if (vehicle) {
+    switch (method) {
+      case "GET":
         res.status(200).json(vehicle);
-      } else {
-        res.status(404).json({ message: `vehicle ${id} not found` });
-      }
-      break;
-    case "DELETE":
-      res.status(200).json({ message: "not implement" });
-      break;
-    default:
-      res.setHeader("Allow", ["GET", "DELETE"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+        break;
+      case "DELETE":
+        await deleteVehicle(id);
+        res.status(200).json({ message: `vehicle ${id} deleted` });
+        break;
+      default:
+        res.setHeader("Allow", ["GET", "DELETE"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } else {
+    res.status(404).json({ message: `vehicle ${id} not found` });
   }
 }
