fc57d54268c8d181be10e728fbe389fa34a4f8e9
kanoonsantikul <nitisantikul@gmail.com>
2021-06-05 16:39:44 +0700

    clean code

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function MyApp({ Component, props }) {
-  return <Component {...props} />;
-}
-
-export async function getStaticProps(context) {
-  const sqlite3 = require("sqlite3").verbose();
-
-  let db = new sqlite3.Database(".db/calvary19.db", (err) => {
-    if (err) {
-      console.error(err.message);
-      return {
-        notFound: true,
-      };
-    }
-  });
-
-  console.log("Connected to the database.");
-  return {
-    props: { db },
-  };
-}
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
 MyApp.defaultProps = {
   Component: null,
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
-export default function Home() {
+const Home = () => {
   return "hello world";
-}
+};
+
+export default Home;
