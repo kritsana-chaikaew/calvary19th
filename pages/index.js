7a703ced8f21d28c0b72f5770b6c0bcb85513e44
kanoonsantikul <nitisantikul@gmail.com>
2021-06-06 14:00:34 +0700

    get vehicles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { Card, List } from "antd";
+
+import "antd/dist/antd.css";
+
 const Home = () => {
-  return "hello world";
+  const [vehicles, setVehicles] = useState([]);
+
+  const getVehicles = useCallback(() => {
+    fetch("/api/vehicles").then((res) => {
+      res.json().then((res) => {
+        setVehicles(vehicles.concat(res));
+      });
+    });
+  }, [vehicles]);
+
+  useEffect(() => {
+    if (vehicles.length === 0) {
+      getVehicles();
+    }
+  });
+
+  return (
+    <List
+      dataSource={vehicles}
+      renderItem={(item) => (
+        <Card title={item?.serial_no} bordered style={{ width: 300 }}>
+          <p>{item?.type}</p>
+          <p>{item?.status}</p>
+          <p>{item?.regimental}</p>
+        </Card>
+      )}
+    />
+  );
 };
 
 export default Home;
