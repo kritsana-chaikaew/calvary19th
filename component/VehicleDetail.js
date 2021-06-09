b973e437757486a789c57acf0d71cb4ae344b5fe
Kritsana <kritsana.chai@ku.th>
2021-06-09 16:09:31 +0700

    vehicle detail

--- component/VehicleDetail.js
+++ component/VehicleDetail.js
@@ -1,31 +1,68 @@
 import React from "react";
-import { Modal, Image } from "antd";
+import { Modal, Image, Row, Col, Tag, Button } from "antd";
+import {
+  CloseCircleOutlined,
+  CheckCircleOutlined,
+  MinusCircleOutlined,
+} from "@ant-design/icons";
 import PropTypes from "prop-types";
 import { statuses } from "../utils/const";
 
-const VehicleDetail = ({ vehicleData, ...rest }) => {
+const VehicleDetail = ({ vehicleData, onOk, ...rest }) => {
+  let color = "success";
+  let icon = <CheckCircleOutlined />;
+  if (vehicleData?.status === statuses[1]) {
+    color = "error";
+    icon = <CloseCircleOutlined />;
+  }
+  if (vehicleData?.status === statuses[2]) {
+    color = "default";
+    icon = <MinusCircleOutlined />;
+  }
   return (
-    <Modal {...rest}>
-      <pre>
-        ภาพ <Image width={100} src={vehicleData?.image} />
-      </pre>
-      <pre>หมายเลข {vehicleData?.serial_no}</pre>
-      <pre>ชนิด {vehicleData?.type}</pre>
-      <pre>สถานะ {vehicleData?.status}</pre>
-      <pre>กองร้อย {vehicleData?.regimental}</pre>
-      {vehicleData?.status === statuses[1] && (
-        <pre>
-          ใบส่งซ่อม <Image width={100} src={vehicleData?.repair_slip} />
-        </pre>
-      )}
+    <Modal
+      {...rest}
+      footer={[
+        <Button key="นา" onClick={onOk} type="primary">
+          ตกลง
+        </Button>,
+      ]}
+    >
+      <Row>
+        <Col span={10}>
+          <Image width={120} height={100} src={vehicleData?.image} />
+        </Col>
+        <Col span={6}>
+          <Row>หมายเลข</Row>
+          <Row>สถานะ</Row>
+          <Row>ชนิด</Row>
+          <Row>กองร้อย</Row>
+          <Row>ใบส่งซ่อม</Row>
+        </Col>
+        <Col span={8}>
+          <Row>{vehicleData.serial_no}</Row>
+          <Row>
+            <Tag icon={icon} color={color}>
+              {vehicleData?.status}
+            </Tag>
+          </Row>
+          <Row>{vehicleData?.type}</Row>
+          <Row>{vehicleData?.regimental}</Row>
+          <Row>
+            <Image width={100} src={vehicleData?.repair_slip} />
+          </Row>
+        </Col>
+      </Row>
     </Modal>
   );
 };
 VehicleDetail.defaultProps = {
   vehicleData: null,
+  onOk: null
 };
 VehicleDetail.propTypes = {
   vehicleData: PropTypes.objectOf(PropTypes.string.isRequired),
+  onOk: PropTypes.func
 };
 
 export default VehicleDetail;
