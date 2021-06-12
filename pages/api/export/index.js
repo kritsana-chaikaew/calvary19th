ae62bdca358a8ceac58d0d65e392838c3afae924
Kritsana <kritsana.chai@ku.th>
2021-06-12 10:45:39 +0700

    export data without writing

--- pages/api/export/index.js
+++ pages/api/export/index.js
@@ -2,20 +2,16 @@ import {
   getVehicles,
 } from "../../../models/Vehicle";
 
-const fs = require("fs");
-const path = require("path");
 const json2xls = require("json2xls");
 
 export default async function handler(req, res) {
   const { method } = req;
   const vehicles = await getVehicles();
   const xls = json2xls(vehicles);
-  const filePath = path.resolve(".", "public/data.xlsx");
-  const fileBuffer = fs.readFileSync(filePath);
+  const fileBuffer = Buffer.from(xls, "binary");
   switch (method) {
     case "GET":
       res.setHeader("content-disposition", "attachment; filename=data.xlsx");
-      fs.writeFileSync(filePath, xls, "binary");
       res.send(fileBuffer);
       break;
     default:
