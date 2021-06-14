971dff08d580915f17a648430c0d79cb2ee0077a
Kritsana <kritsana.chai@ku.th>
2021-06-14 20:42:20 +0700

    upload api

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -1,14 +1,46 @@
-const formidable = require("formidable");
+import { IncomingForm } from "formidable";
+import { promises as fs } from "fs";
 
-const handler = async (req, res) => {
-  const { method } = req;
-  const form = new formidable.IncomingForm({ keepExtensions: true });
-  form.parse(req, (err, fields, files) => {
-    console.log(err, fields, files);
+const path = require("path");
+
+const upload = async (req) => {
+  const { files } = await new Promise((resolve, reject) => {
+    const form = new IncomingForm({keepExtensions: true});
+    form.parse(req, (err, fields, files) => {
+      if (err) return reject(err);
+      return resolve({fields, files});
+    });
   });
+  const tempPath = files.file.path;
+  const fileName = path.basename(tempPath);
+  const urlPath = path.join("/uploads", fileName.toLocaleLowerCase());
+  const destinationPath = path.join("./public", urlPath);
+  console.log(urlPath, destinationPath);
+  fs.rename(tempPath, destinationPath, (err) => {
+    if (err) console.log(err);
+    return {
+      "name": "",
+      "status": "error",
+      "url": "",
+      "thumbUrl": ""
+    };
+  });
+  return {
+    "name": fileName,
+    "status": "done",
+    "url": urlPath,
+    "thumbUrl": urlPath
+  };
+};
+
+export default async (req, res) => {
+  const { method } = req;
+  let data;
   
   switch (method) {
     case "POST":
+      data = await upload(req);
+      res.status(200).json(data);
       break;
     default:
       res.setHeader("Allow", ["POST"]);
@@ -21,5 +53,3 @@ export const config = {
     bodyParser: false,
   },
 };
-
-export default handler;
