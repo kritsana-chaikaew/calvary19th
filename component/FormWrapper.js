80080e74654b60d1b35921c92897545536ecf83c
Kritsana <kritsana.chai@ku.th>
2021-06-14 14:50:34 +0700

    garage options

--- component/FormWrapper.js
+++ component/FormWrapper.js
@@ -6,12 +6,15 @@ import {
   MinusCircleOutlined,
 } from "@ant-design/icons";
 import PropTypes from "prop-types";
-import { statuses, types, regimentals } from "../utils/const";
+import { statuses, types, regimentals, garages } from "../utils/const";
 import ImageUpload from "./ImageUpload";
 
 const statusOptions = statuses.map((status) => ({ value: status }));
 const typeOptions = types.map((type) => ({ value: type.name }));
-const regimentalOptions = regimentals.map((regimental) => ({ value: regimental }));
+const regimentalOptions = regimentals.map((regimental) => ({
+  value: regimental,
+}));
+const garageOptions = garages.map((garage) => ({ value: garage.name }));
 
 const tagRender = (props) => {
   const { label, value, onClose } = props;
@@ -74,7 +77,6 @@ const FormWrapper = ({
           disabled={!isEdit}
           style={{ width: "100%" }}
           options={typeOptions}
-          placeholder="ชนิด"
         />
       </Form.Item>
       <Form.Item label="สถานะ" name="status" rules={[{ required: isEdit }]}>
@@ -91,16 +93,23 @@ const FormWrapper = ({
       <Form.Item label="หมายเหตุ" name="symptom" rules={[{ required: false }]}>
         <Input.TextArea readOnly={!isEdit} type="text" placeholder="หมายเหตุ" />
       </Form.Item>
-      <Form.Item label="กองร้อย" name="regimental" rules={[{ required: isEdit }]}>
+      <Form.Item
+        label="กองร้อย"
+        name="regimental"
+        rules={[{ required: isEdit }]}
+      >
         <Select
           disabled={!isEdit}
           style={{ width: "100%" }}
           options={regimentalOptions}
-          placeholder="กองร้อย"
         />
       </Form.Item>
       <Form.Item label="อยู่ที่" name="garage" rules={[{ required: isEdit }]}>
-        <Input readOnly={!isEdit} type="text" placeholder="อยู่ที่" />
+        <Select
+          disabled={!isEdit}
+          style={{ width: "100%" }}
+          options={garageOptions}
+        />
       </Form.Item>
       <Form.Item label="จอดแถวที่" name="row" rules={[{ required: isEdit }]}>
         <Input
